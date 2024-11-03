60e4d9493ea1f1d90ab5cab88227ec3ad87e6dfc
devlow03 <nth4356@gmail.com>
2024-11-03 13:53:09 +0700

    feat: go to profile in chat page

--- lib/src/features/chat/presentation/pages/chat_page.dart
+++ lib/src/features/chat/presentation/pages/chat_page.dart
@@ -195,83 +195,94 @@ class _ChatPageState extends ConsumerState<ChatPage> {
               final user = snapshot.data;
               return Visibility(
                 visible: controller.user != null,
-                child: Row(
-                  // crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Visibility(
-                        visible: user?['status'] == "online",
-                        replacement: CircleAvatar(
-                          radius: 20,
-                          backgroundImage: CachedNetworkImageProvider(
-                            controller.user?.userAvatar ?? "",
-                          ),
-                        ),
-                        child: Stack(
-                          children: [
-                            // Avatar
-                            CircleAvatar(
+                child: GestureDetector(
+                  onTap: () {
+                    context.pushNamed('profile', queryParameters: {
+                      "userId": controller.user?.userId.toString()
+                    });
+                  },
+                  child: Container(
+                    color: Colors.white,
+                    child: Row(
+                      // crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Visibility(
+                            visible: user?['status'] == "online",
+                            replacement: CircleAvatar(
                               radius: 20,
                               backgroundImage: CachedNetworkImageProvider(
                                 controller.user?.userAvatar ?? "",
                               ),
                             ),
-                            // Chấm tròn cắt vào avatar
-                            Positioned(
-                              bottom: 0,
-                              right: 0,
-                              child: Container(
-                                width: 12, // Độ rộng của chấm tròn
-                                height: 12, // Chiều cao của chấm tròn
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.green, // Màu của chấm tròn
-                                  border: Border.all(
-                                    color: Colors
-                                        .white, // Đường viền màu trắng để tạo hiệu ứng cắt vào avatar
-                                    width: 2, // Độ dày của viền
+                            child: Stack(
+                              children: [
+                                // Avatar
+                                CircleAvatar(
+                                  radius: 20,
+                                  backgroundImage: CachedNetworkImageProvider(
+                                    controller.user?.userAvatar ?? "",
                                   ),
                                 ),
-                              ),
-                            ),
-                          ],
-                        )),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          controller.user?.userFullName ?? "",
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
+                                // Chấm tròn cắt vào avatar
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Container(
+                                    width: 12, // Độ rộng của chấm tròn
+                                    height: 12, // Chiều cao của chấm tròn
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.green, // Màu của chấm tròn
+                                      border: Border.all(
+                                        color: Colors
+                                            .white, // Đường viền màu trắng để tạo hiệu ứng cắt vào avatar
+                                        width: 2, // Độ dày của viền
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )),
                         const SizedBox(
-                          height: 5,
+                          width: 10,
                         ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             Text(
-                              user?['status'] == "online"
-                                  ? "Đang hoạt động"
-                                  : AppUtils.formatTimeMessage(
-                                      user?['updatedAt']),
-                              // AppUtils.formatTimeMessage(controller.user?.leftAt),
-                              style: const TextStyle(
-                                  color: Colors.black45, fontSize: 12),
+                              controller.user?.userFullName ?? "",
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
                             ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  user?['status'] == "online"
+                                      ? "Đang hoạt động"
+                                      : AppUtils.formatTimeMessage(
+                                          user?['updatedAt']),
+                                  // AppUtils.formatTimeMessage(controller.user?.leftAt),
+                                  style: const TextStyle(
+                                      color: Colors.black45, fontSize: 12),
+                                ),
+                              ],
+                            )
                           ],
                         )
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               );
             },
           ),
-          actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
+          // actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.menu))],
         ));
   }
 }
