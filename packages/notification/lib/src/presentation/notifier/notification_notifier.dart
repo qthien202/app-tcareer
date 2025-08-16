f4eb3d3f8ac5a0e62b9f0ba7468299c3270fa70f
qthien202 <nth4356@gmail.com>
2025-08-16 20:49:45 +0700

    fix[notification]: listen notification in notifier

--- packages/notification/lib/src/presentation/notifier/notification_notifier.dart
+++ packages/notification/lib/src/presentation/notifier/notification_notifier.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
 
 import 'package:core/core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:notification/src/domain/entities/notification_entity.dart';
-
 import 'package:notification/src/domain/usecases/listen_notification_usecase.dart';
 import 'package:notification/src/domain/usecases/read_notification_usecase.dart';
 
@@ -17,58 +14,65 @@ class NotificationNotifier extends AsyncNotifier<NotificationState> {
   late final UserUtils _userUtils;
   late final ReadNotificationUseCase _readNotificationUseCase;
   StreamSubscription<List<NotificationEntity>>? _subscription;
-  NotificationNotifier();
 
   @override
   Future<NotificationState> build() async {
     _listenNotificationUseCase = ref.read(listenNotificationUseCaseProvider);
     _userUtils = ref.read(userUtilsProvider);
     _readNotificationUseCase = ref.read(readNotificationUseCaseProvider);
+
+    state = const AsyncValue.data(NotificationState(isLoading: true));
+
+    _subscription?.cancel();
+
+    ref.onDispose(() {
+      _subscription?.cancel();
+    });
+
     await _listenNotification();
-    return state.value ?? const NotificationState();
+
+    return const NotificationState(isLoading: true);
   }
 
   Future<void> _listenNotification() async {
-    state = const AsyncValue.loading();
-    final userId = await _userUtils.getUserId();
-    if (userId.isEmpty) {
-      state = const AsyncValue.data(NotificationState());
-      return;
-    }
-    _subscription = _listenNotificationUseCase(userId).listen((notifications) {
-      state = AsyncValue.data(NotificationState(
-          notifications: notifications, isLoading: false, errorMessage: null));
-    }, onError: (error, stackTrace) {
+    try {
+      final userId = await _userUtils.getUserId();
+
+      if (userId.isEmpty) {
+        state = const AsyncValue.data(NotificationState(
+            notifications: [], isLoading: false, errorMessage: null));
+        return;
+      }
+
+      _subscription = _listenNotificationUseCase(userId).listen(
+        (notifications) {
+          if (!ref.exists(notificationNotifierProvider)) return;
+
+          state = AsyncValue.data(NotificationState(
+              notifications: notifications,
+              isLoading: false,
+              errorMessage: null));
+        },
+        onError: (error, stackTrace) {
+          if (!ref.exists(notificationNotifierProvider)) return;
+
+          state = AsyncValue.data(NotificationState(
+              notifications: state.value?.notifications ?? [],
+              isLoading: false,
+              errorMessage: error.toString()));
+        },
+      );
+    } catch (error, stackTrace) {
       state = AsyncValue.error(error, stackTrace);
-    });
+    }
   }
 
   Future<void> readNotification(String notificationId) async {
     await _readNotificationUseCase(notificationId);
   }
 
-  Future<void> directToPage(
-      {required BuildContext context,
-      required NotificationEntity notification}) async {
-    readNotification(notification.id);
-    final isNotificationPost = notification.postId != null;
-    final isNotificationProfile = notification.relatedUserId != null;
-    final isNotificationComment =
-        notification.type == NotificationType.COMMENT &&
-            notification.postId != null;
-    if (isNotificationPost) {
-      context.pushNamed("detail",
-          pathParameters: {"id": notification.postId.toString()});
-    }
-    if (isNotificationProfile) {
-      context.pushNamed('profile',
-          queryParameters: {"userId": notification.relatedUserId.toString()});
-    }
-    if (isNotificationComment) {
-      context.pushNamed("detail",
-          pathParameters: {"id": notification.postId.toString()},
-          queryParameters: {"notificationType": notification.type.name});
-    }
+  Future<void> refresh() async {
+    await _listenNotification();
   }
 }
 
