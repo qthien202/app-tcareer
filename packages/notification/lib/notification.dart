5be6667d1e2559737efde5cbe0a00c89c02e2e96
qthien202 <nth4356@gmail.com>
2025-08-16 21:54:23 +0700

    chore: add notification

---
+++ packages/notification/lib/notification.dart
@@ -0,0 +1,4 @@
+export 'src/presentation/ui/notification_page.dart';
+export 'src/domain/entities/notification_entity.dart';
+export 'src/presentation/notifier/notification_notifier.dart';
+export 'src/presentation/ui/widgets/notification_icon_widget.dart';
